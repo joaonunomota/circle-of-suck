bcb07b7cb384520f33b8fb2f4aaca4d8c8cd28c4
João Nuno Mota <jnuno.carrilho@gmail.com>
2020-11-30 20:25:39 +0000

    Revert removal of GETs to football-data API

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# curl -H 'X-Auth-Token: $0' -X GET http://api.football-data.org/v2/competitions/2017/matches?status=FINISHED > ./src/json/primeiraLiga.json && \
-# curl -H 'X-Auth-Token: $0' -X GET http://api.football-data.org/v2/competitions/2021/matches?status=FINISHED > ./src/json/premierLeague.json && \
+curl -H 'X-Auth-Token: $0' -X GET http://api.football-data.org/v2/competitions/2017/matches?status=FINISHED > ./src/json/primeiraLiga.json && \
+curl -H 'X-Auth-Token: $0' -X GET http://api.football-data.org/v2/competitions/2021/matches?status=FINISHED > ./src/json/premierLeague.json && \
 yarn build
