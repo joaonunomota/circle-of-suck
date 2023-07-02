4d8ad14c82b29c9cd7367b1740348f881918683a
João Nuno Mota <jnuno.carrilho@gmail.com>
2023-07-02 15:16:07 +0100

    Update storybook style to match production code

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,12 +2,14 @@ import type { Preview } from "@storybook/vue3";
 import { useArgs } from "@storybook/client-api";
 
 import "bulma/css/bulma.min.css";
+import "../src/assets/main.scss";
 
 const decorators = [
   (story, context) => {
     const [_, updateArgs] = useArgs();
     return story({ ...context, updateArgs });
-  }
+  },
+  () => ({ template: '<div id="app"><story/></div>' }),
 ];
 
 const preview: Preview = {
